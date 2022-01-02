9149ca4149ea5cade054ce16e9fff5e9320d60b5
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-01-02 15:42:02 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Your code here
-let timeInEvents = []
 
 function createEmployeeRecord([first, last, title, pay]){
     let employee = {
@@ -16,8 +15,47 @@ function createEmployeeRecords(name){
     let array = Array.from(name)
     return array.map(createEmployeeRecord) 
 }
-function createTimeInEvent(object, date){
-    createEmployeeRecord(object)
-    timeInEvents.date = date.slice(0,9)
-    timeInEvents.hour = date.slice(10)
+function createTimeInEvent(object, time){
+    let date = time.split(' ')[0]
+    let hour = time.split(' ')[1]
+    let inTime = {
+        hour: parseInt(hour),
+        date: date,
+        type: 'TimeIn'
+    }
+    object.timeInEvents.push(inTime)
+    return object
+}
+function createTimeOutEvent(object, time){
+    let date = time.split(' ')[0]
+    let hour = time.split(' ')[1]
+    let outTime = {
+        hour: parseInt(hour),
+        date: date,
+        type: 'TimeOut'
+    }
+    object.timeOutEvents.push(outTime)
+    return object
+}
+function hoursWorkedOnDate(object){
+    for(let i=0; i< object.timeInEvents.length; i++){
+        for(let j=0; j< object.timeOutEvents.length; j++){
+            let timeIn = object.timeInEvents[i].date
+            let timeOut = object.timeOutEvents[j].date
+            let hourIn = object.timeInEvents[i].hour
+            let hourOut = object.timeOutEvents[j].hour
+            if(timeIn === timeOut){
+                return ((hourOut - hourIn)/100)
+            }else if(timeIn < timeOut){
+                return (((hourIn-2400)+ hourOut)/100)
+            }
+        }
+    }
+}
+function wagesEarnedOnDate(object){
+    let totalHours = hoursWorkedOnDate(object)
+    return (totalHours * object.payPerHour)
+}
+function allWagesFor(object){
+    console.log(object.timeInEvents.timeOutEvents)
 }
