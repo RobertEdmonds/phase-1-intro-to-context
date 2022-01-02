3e4d31a0ffed0f5269f03ef2a18dce767d54e040
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-01-02 12:07:37 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+let timeInEvents = []
+
+function createEmployeeRecord([first, last, title, pay]){
+    let employee = {
+        firstName: first,
+        familyName: last,
+        title: title,
+        payPerHour: pay
+    }
+    employee.timeInEvents = []
+    employee.timeOutEvents = []
+    return employee
+}
+function createEmployeeRecords(name){
+    let array = Array.from(name)
+    return array.map(createEmployeeRecord) 
+}
+function createTimeInEvent(object, date){
+    createEmployeeRecord(object)
+    timeInEvents.date = date.slice(0,9)
+    timeInEvents.hour = date.slice(10)
+}
